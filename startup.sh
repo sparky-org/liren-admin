a938af35282e3c24ad7d23774fc8b248a85cbd46
jdyuankai <yuank1987@163.com>
2019-11-25 16:51:06 +0800

    fix 启动脚本

--- startup.sh
+++ startup.sh
@@ -6,6 +6,11 @@ mvn clean install
 ## shutdown
 lsof -i:8088 | awk '/java/ {print $2}'|xargs kill -9
 
+sleep 1
+
+## check pid stopped
+lsof -i:8088
+
 ## starup
 nohup java -jar ./target/liren-admin-0.0.1-SNAPSHOT.jar &>liren.log &
 
