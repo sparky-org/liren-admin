e144de25cf006d776f9b7be377a75c503878d126
jdyuankai <yuank1987@163.com>
2019-12-09 19:03:27 +0800

    fix bug gongzuo jifen

--- src/main/java/com/sparky/lirenadmin/constant/PointTypeEnum.java
+++ src/main/java/com/sparky/lirenadmin/constant/PointTypeEnum.java
@@ -2,7 +2,7 @@ package com.sparky.lirenadmin.constant;
 
 public enum PointTypeEnum {
     CHARACTER("CHARACTER", "品德积分"),
-    ACTION("ACTION", "行为积分"),
+    ACTION("ACTION", "工作积分"),
     DAILY_RECORD("DAILY_RECORD", "日记奖励"),
     APPOINTMENT("APPOINTMENT", "预约客户奖励"),
     ATTENDANCE("ATTENDANCE", "按时打卡奖励");
